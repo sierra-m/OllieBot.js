ecccb12cd9657be71afe4653975de6b53766344d
Sierra MacLeod <sierra.m.macleod@gmail.com>
2020-09-07 04:45:26 -0700

    misc test

--- src/util/handle-misc.js
+++ src/util/handle-misc.js
@@ -6,7 +6,9 @@
 export default async function handleMisc (bot, message) {
   // Shoe
   if (message.guild === '313841769441787907') {
+    console.log('Is Shoe.');
     if (message.hasMedia()) {
+      console.log('Has media.');
       // disco-memes, emote submissions
       if (['498444135615692820', '607765472439697418'].includes(message.channel)) {
         await message.react('👍');
